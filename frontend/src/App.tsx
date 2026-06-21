f4590edb96b800109fc707554aa09810d1067ce9
Subham Gautam <gautamsubham65@gmail.com>
2026-06-21 22:51:03 +0545

    (fix): public routes and protected routes different

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,10 +5,14 @@ import Dashboard from './pages/Dashboard'
 import Applications from './pages/Applications'
 import AddApplication from './pages/AddApplication'
 import Settings from './pages/Settings'
+import Signup from './pages/auth/Signup';
+import Login from './pages/auth/Login';
 
 export default function AppRoutes() {
   return (
     <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
       <Route element={<MainLayout />}>
         <Route path="/" element={<Dashboard />} />
         <Route path="/applications" element={<Applications />} />
